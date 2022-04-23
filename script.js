4e19d545fc2e69169f7bdcd8b5ff536d672f3e58
blevinspercussion <blevinspercussion@gmail.com>
2022-04-23 15:02:12 -0500

    refactored playRound() function

--- script.js
+++ script.js
@@ -4,61 +4,127 @@ function computerPlay() {
     return rps[turn];
 }
 
-function playRound() {
-    let playerTurn = prompt("Choose rock, paper, or scissors: ").toLowerCase();
-    let computerTurn = computerPlay();
-    if (playerTurn === 'rock') {
-        switch (computerTurn) {
+function playRound(computerSelection, playerSelection) {
+    if (playerSelection === 'rock') {
+        switch (computerSelection) {
             case 'rock':
-                console.log("Computer chooses rock...")
-                console.log('Draw! Play again!');
-                playRound();
+                return 'Computer chooses rock...\nDraw! Play again!';
+                break;
 
             case 'paper':
-                console.log("Computer chooses paper...")
-                console.log('Paper covers rock. You lose!');
+                return 'Computer chooses paper...\nPaper covers rock.\nYou lose!';
                 break;
 
             case 'scissors':
-                console.log("Computer chooses scissors...")
-                console.log('Rock breaks scissors. You win!');
+                return 'Computer chooses scissors...\nRock smashes scissors.\nYou win!';
                 break;
+
         }
-    } else if (playerTurn === 'paper') {
-        switch (computerTurn) {
+
+    } else if (playerSelection === 'paper') {
+        switch (computerSelection) {
             case 'rock':
-                console.log("Computer chooses rock...")
-                console.log('Paper covers rock. You win!');
+                return 'Computer chooses rock...\nPaper covers rock.\nYou win!';
                 break;
 
             case 'paper':
-                console.log("Computer chooses paper...")
-                console.log('Draw! Play again!');
-                playRound();
+                return 'Computer chooses paper...\nDraw! Play again!';
+                break;
 
             case 'scissors':
-                console.log("Computer chooses scissors...")
-                console.log('Scissors cut paper. You lose!');
+                return 'Computer chooses scissors...\nScissors cut paper.\nYou lose!';
                 break;
+
         }
-    } else if (playerTurn === 'scissors') {
-        switch (computerTurn) {
+
+    } else if (playerSelection === 'scissors') {
+        switch (computerSelection) {
             case 'rock':
-                console.log("Computer chooses rock...")
-                console.log("Rock breaks scissors. You lose!");
+                return 'Computer chooses rock...\nRock smashes scissors.\nYou lose!';
                 break;
 
             case 'paper':
-                console.log("Computer chooses paper...")
-                console.log("Scissors cut paper. You win!");
+                return 'Computer chooses paper...\nScissors cut paper.\nYou win!';
                 break;
 
             case 'scissors':
-                console.log("Computer chooses scissors...")
-                console.log("Draw! Play again!");
-                playRound();
+                return 'Computer chooses scissors...\nDraw! Play again!';
+                break;
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function playRound(playerSelection, computerSelection) {
+//     let playerTurn = prompt("Choose rock, paper, or scissors: ").toLowerCase();
+//     let computerTurn = computerPlay();
+//     if (playerTurn === 'rock') {
+//         switch (computerTurn) {
+//             case 'rock':
+//                 console.log("Computer chooses rock...")
+//                 console.log('Draw! Play again!');
+//                 playRound();
+
+//             case 'paper':
+//                 console.log("Computer chooses paper...")
+//                 console.log('Paper covers rock. You lose!');
+//                 break;
+
+//             case 'scissors':
+//                 console.log("Computer chooses scissors...")
+//                 console.log('Rock breaks scissors. You win!');
+//                 break;
+//         }
+//     } else if (playerTurn === 'paper') {
+//         switch (computerTurn) {
+//             case 'rock':
+//                 console.log("Computer chooses rock...")
+//                 console.log('Paper covers rock. You win!');
+//                 break;
+
+//             case 'paper':
+//                 console.log("Computer chooses paper...")
+//                 console.log('Draw! Play again!');
+//                 playRound();
+
+//             case 'scissors':
+//                 console.log("Computer chooses scissors...")
+//                 console.log('Scissors cut paper. You lose!');
+//                 break;
+//         }
+//     } else if (playerTurn === 'scissors') {
+//         switch (computerTurn) {
+//             case 'rock':
+//                 console.log("Computer chooses rock...")
+//                 console.log("Rock breaks scissors. You lose!");
+//                 break;
+
+//             case 'paper':
+//                 console.log("Computer chooses paper...")
+//                 console.log("Scissors cut paper. You win!");
+//                 break;
+
+//             case 'scissors':
+//                 console.log("Computer chooses scissors...")
+//                 console.log("Draw! Play again!");
+//                 playRound();
+//         }
+//     }
+// }
 
 playRound();
